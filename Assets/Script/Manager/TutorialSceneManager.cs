e5203acb9502dcc54da2ee7468de03e153ddc7cb
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-02-08 19:30:08 +0900

    RecvBuffer 완료

---
+++ Assets/Script/Manager/TutorialSceneManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 튜토리얼을 진행하기 위해서 랜덤하지 않게 임의의 위치에 포션을 배치하자.
+
+/*
+1. 규칙 설명
+   15초 내에 최대한 많은 포션을 파괴해 상대보다 강한 마법을 구현하자!
+
+2. 총 5개의 일반 포션이 있다. (다음으로)
+
+3. 붉은 색의 포션을 이어봅시다!
+
+4. 5개 이어서 특수 포션을 만들어보자!
+
+5. 7개 이어서 특수 포션을 만들어보자!
+
+6. 10개 이어서 특수 포션을 만들어보자!
+
+7. 핸드폰을 흔들어서 포션들을 정돈해보자!
+
+8. 상대방의 체력을 0으로 만들어보자!
+ 
+ */
+public class TutorialSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
